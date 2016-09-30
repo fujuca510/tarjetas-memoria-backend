6d9025a16367d8a12c4e96a38176ec4fa9f33e48
Juan Carlos Huarachi <jhuarachi@geo.gob.bo>
2016-09-30 11:49:42 -0400

    Mostrar los errores en los servicios

--- routes/tarjetas.js
+++ routes/tarjetas.js
@@ -3,13 +3,23 @@ var router = express.Router();
 
 var tarjetaService = require('../services/tarjetaService');
 
-/* GET users listing. */
+/**
+* GET tarjetas -> obtiene la lista de tarjetas.
+* @name /api/v1/tarjetas
+*/
 router.get('/', (req, res) => {
   tarjetaService.obtenerTodos()
     .then((tarjetas) => res.json(tarjetas))
-    .catch((error) => res.status(500).json({mensaje: 'Error en obtener tarjetas'}));
+    .catch((error) => {
+      console.log(error.stack);
+      res.status(500).json({mensaje: 'Error en obtener tarjetas'});
+    });
 });
 
+/**
+* GET tarjetas/:id -> Obtiene el objeto tarjeta por ID
+* @name /api/v1/tarjetas/:id,  donde :id es el identificador de la tarjeta
+*/
 router.get('/:id', (req, res) => {
   var tarjetaId = req.params.id;
   tarjetaService.obtenerPorId(tarjetaId)
@@ -19,9 +29,22 @@ router.get('/:id', (req, res) => {
       else
         res.status(403).json({mensaje: 'Tarjeta no existe'});
     })
-    .catch((error) => res.status(500).json({mensaje: 'Error en obtener tarjeta'}));
+    .catch((error) => {
+      console.log(error.stack);
+      res.status(500).json({mensaje: 'Error en obtener tarjeta'});
+    });
 });
 
+/**
+* POST tarjetas -> Servicio para crear una tarjeta
+* @name /api/v1/tarjetas
+* Parametros del servicio en el cuerpo de la petición
+* {
+  	"texto_referencia":"CIENTIFICO",
+  	"texto_memoria":"SCIENTIST",
+  	"grupo":"INGLES-PROFESIONES"
+* }
+*/
 router.post('/', (req, res) => {
   var objeto = {
     texto_referencia: req.body.texto_referencia,
@@ -30,9 +53,22 @@ router.post('/', (req, res) => {
   }
   tarjetaService.crear(objeto)
     .then((tarjetaNueva) => res.json(tarjetaNueva))
-    .catch((error) => res.status(500).json({mensaje: 'Error en registrar tarjeta'}));
+    .catch((error) => {
+      console.log(error.stack);
+      res.status(500).json({mensaje: 'Error en registrar tarjeta'})
+    });
 });
 
+/**
+* PUT tarjetas/:id -> Servicio para actualizar una tarjeta, por identificador de la tarjeta
+* @name /api/v1/tarjetas/:id, donde :id es el identificador de la tarjeta
+* Parametros del servicio en el cuerpo de la petición
+* {
+  	"texto_referencia":"CIENTIFICO",
+  	"texto_memoria":"SCIENTIST",
+  	"grupo":"INGLES-PROFESIONES"
+* }
+*/
 router.put('/:id', (req, res) => {
   var tarjetaId = req.params.id;
   var objeto = {
@@ -42,14 +78,25 @@ router.put('/:id', (req, res) => {
   }
   tarjetaService.actualizarPorId(tarjetaId, objeto)
     .then((tarjetaNueva) => res.json(tarjetaNueva))
-    .catch((error) => res.status(500).json({mensaje: 'Error en actualizar tarjeta'}));
+    .catch((error) => {
+      console.log(error.stack);
+      res.status(500).json({mensaje: 'Error en actualizar tarjeta'})
+    });
 });
 
+/**
+* DELETE tarjetas/:id -> Servicio para eliinar una tarjeta, por identificador de la tarjeta
+* @name /api/v1/tarjetas/:id, donde :id es el identificador de la tarjeta
+* Nota el valor de :id, solo acepta números ver validación con expresión regular ([0-9]+)
+*/
 router.delete('/:id([0-9]+)', (req, res) => {
   var tarjetaId = req.params.id;
   tarjetaService.eliminarPorId(tarjetaId)
     .then((tarjetaEliminada) => res.json(tarjetaEliminada))
-    .catch((error) => res.status(500).json({mensaje: 'Error en eliminar tarjeta'}));
+    .catch((error) => {
+      console.log(error.stack);
+      res.status(500).json({mensaje: 'Error en eliminar tarjeta'})
+    });
 });
 
 module.exports = router;
